a510e6f4c9f0e454408d888dd4432731afe794b4
Shubham <realitymaniac7@gmail.com>
2025-02-20 23:41:14 +0530

    size change

--- src/App.jsx
+++ src/App.jsx
@@ -34,16 +34,16 @@ const App = () => {
   }
 
   return (
-    <div className="bg-gray-800 h-auto p-5 my-10 w-full max-w-md mx-auto rounded-xl text-white shadow-xl overflow-hidden flex flex-col ">
+    <div className="bg-gray-800 max-h-auto h-62 p-5 my-10 m-w-full w-[48rem] mx-auto rounded-xl text-white shadow-xl overflow-hidden flex flex-col ">
 
-      <h1 className="mx-auto mb-4">Password generator</h1>
-      <div className="overflow-hidden rounded-md mx-auto flex w-full" name="password display with button">
+      <h1 className="mx-auto mb-10 mt-2 text-3xl cursor-default font-semibold font-mono [text-shadow:_0_2px_4px_rgb(99_102_241_/_0.8)]" id="heading">Password generator</h1>
+      <div className=" rounded-md mx-4 flex w-auto mb-2" name="password display with button">
         <input
           type="text"
-          className="bg-white px-2  text-black flex-1 mx-none rounded-l-md outline-none"
+          className="bg-white px-4 p-3 h-12 italic text-xl text-black flex-1 mx-none rounded-l-md outline-none max-w-auto w- "
           value={password}
         />
-        <button className="bg-blue-500 px-2 rounded-r-md cursor-pointer" onClick={() => copy()}> Copy </button>
+        <button className="bg-blue-500 px-2 rounded-r-md cursor-pointer w-18 text-lg" onClick={() => copy()}> Copy </button>
       </div>
 
       <div className="mt-5 flex justify-between mb">
@@ -51,7 +51,7 @@ const App = () => {
         <div className="flex items-center">
           <input
             type="range"
-            className="h-1 my-2.5 mr-2 cursor-pointer"
+            className="h-1 my-2.5 mr-4 ml-5 cursor-pointer "
             min={8}
             max={30}
             value={length}
@@ -62,28 +62,28 @@ const App = () => {
               showpassword()
             }}
           />
-          <label htmlFor="length" className="text-sm " >Length: {length}</label>
+          <label htmlFor="length" className="text-xl" >Length : {length}</label>
         </div>
 
         <div className="flex items-center" name="checkbox for nums">
 
-          <input type="checkbox" name="cbn" id="cbn" 
+          <input type="checkbox" name="cbn" id="cbn" className="w-5 h-5 mr-1 cursor-pointer"
           defaultChecked={numbers} 
           onChange={() => setNumbers((prev) => !prev)}/>
 
-          <label htmlFor="cbn" className="text-sm ml-1">
+          <label htmlFor="cbn" className="text-xl ml-1 ">
             Numbers
           </label>
 
         </div>
 
-        <div className="flex items-center" name="checkbox for chars">
+        <div className="flex items-center mr-4" name="checkbox for chars">
 
-          <input type="checkbox" name="cbc" id="cbc" 
+          <input type="checkbox" name="cbc" id="cbc" className="w-5 h-5 mr-1 cursor-pointer"
           defaultChecked={characters} 
           onChange={() => setCharacters((prev) => !prev)}/>
 
-          <label htmlFor="cbc" className="ml-1 text-sm">
+          <label htmlFor="cbc" className="ml-1 text-xl">
             Characters
           </label>
 
